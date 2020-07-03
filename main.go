ba4864beb23b32c07df8fcf52e308ce3c03fd63c
how8570 <joe13512345@gmail.com>
2020-07-03 20:34:28 +0800

    done handel event/names

--- main.go
+++ main.go
@@ -176,11 +176,49 @@ func handlePunch(w http.ResponseWriter, r *http.Request) {
 // 	return err == nil
 // }
 
+type event struct {
+	eventUUID string
+	title     string
+	content   string
+}
+
 func handleEventNames(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 	w.Header().Set("Content-Type", "application/json")
 
+	db, err := sql.Open("sqlite3", "./data/database.sqlite3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT UUID, title, content FROM Event ORDER BY id DESC;")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var e event
+	var events []event
+
+	for rows.Next() {
+		err = rows.Scan(&e.eventUUID, &e.title, &e.content)
+		events = append(events, e)
+	}
+
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	response, err := json.Marshal(events)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintln(w, string(response))
 }
 
 type eventPoints struct {
@@ -261,12 +299,12 @@ func handleEventPoints(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var json = jsoniter.ConfigCompatibleWithStandardLibrary
-	asd, err := json.Marshal(ep)
+	response, err := json.Marshal(ep)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Fprintln(w, string(asd))
+	fmt.Fprintln(w, string(response))
 }
 
 func init() {
